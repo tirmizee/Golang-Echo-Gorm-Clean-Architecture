63b309bfcc7bf3b830480dc4b72ffa9e2f7df512
Pratya Yeekhaday <pratya.yeekhaday@accenture.com>
2022-01-19 23:14:50 +0700

    init handler

--- features/role/handler.go
+++ features/role/handler.go
@@ -1,1 +1,17 @@
 package role
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type Handler struct{}
+
+func NewHandler() *Handler {
+	return &Handler{}
+}
+
+func (h *Handler) AllUserHandler(c echo.Context) error {
+	return c.String(http.StatusOK, "hello world")
+}
