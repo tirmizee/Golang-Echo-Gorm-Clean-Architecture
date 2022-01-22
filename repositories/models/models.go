7546158f1397a35b2796b13a7dc91c0a2a8e0ec4
Pratya Yeekhaday <pratya.yeekhaday@accenture.com>
2022-01-22 17:52:03 +0700

    update model

--- repositories/models/models.go
+++ repositories/models/models.go
@@ -10,53 +10,53 @@ type BaseModel struct {
 
 type User struct {
 	BaseModel
-	Username  string
-	Password  string
-	Email     string
-	FirstName string
-	LastName  string
+	Username  string `gorm:"column:username"`
+	Password  string `gorm:"column:password"`
+	Email     string `gorm:"column:email"`
+	FirstName string `gorm:"column:first_name"`
+	LastName  string `gorm:"column:last_name"`
 }
 
 type Role struct {
 	BaseModel
-	Code string
-	Name string
-	Desc string
+	Code string `gorm:"column:code"`
+	Name string `gorm:"column:name"`
+	Desc string `gorm:"column:desc"`
 }
 
 type Produc struct {
 	BaseModel
-	Name            string
-	Alias           string
-	SortDescription string
-	FullDescription string
-	CategoryID      uint
-	BrandID         uint
+	Name       string `gorm:"column:name"`
+	Alias      string `gorm:"column:alias"`
+	ShortDesc  string `gorm:"column:short_descr"`
+	FullDesc   string `gorm:"column:full_desc"`
+	CategoryID uint   `gorm:"column:category_id"`
+	BrandID    uint   `gorm:"column:brand_id"`
 }
 
 type ProducDetail struct {
 	BaseModel
-	Name  string
-	Value string
+	Name  string `gorm:"column:name"`
+	Value string `gorm:"column:value"`
 }
 
 type ProductImage struct {
 	BaseModel
-	Name     string
-	Url      string
-	Path     string
-	ProducID uint
+	Name     string `gorm:"column:name"`
+	Url      string `gorm:"column:url"`
+	Path     string `gorm:"column:path"`
+	ProducID uint   `gorm:"column:product_id"`
 }
 
 type Category struct {
 	BaseModel
-	Name  string
-	Alias string
-	Image string
+	Name  string `gorm:"column:name"`
+	Alias string `gorm:"column:alias"`
+	Image string `gorm:"column:image"`
 }
 
 type Brand struct {
 	BaseModel
-	Name string
-	Logo string
+	Name string `gorm:"column:name"`
+	Logo string `gorm:"column:logo"`
 }
